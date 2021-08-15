29931f5188abf7955548dd45c12bc095df35ac92
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-08-15 19:49:45 +0900

    リスト2.5 -> リスト2.7

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,11 +1,92 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+
+struct MemoryMap
+{
+    UINTN buffer_size;
+    VOID *buffer;
+    UINTN map_size;
+    UINTN map_key;
+    UINTN descriptor_size;
+    UINT32 descriptor_version;
+};
+
+EFI_STATUS GetMemoryMap(struct MemoryMap *map)
+{
+    if (map->buffer == NULL)
+    {
+        return EFI_BUFFER_TOO_SMALL;
+    }
+
+    map->map_size = map->buffer_size;
+    return gBS->GetMemoryMap(
+        &map->map_size,
+        (EFI_MEMORY_DESCRIPTOR *)map->buffer,
+        &map->map_key, &map->descriptor_size,
+        &map->descriptor_version);
+}
+
+const CHAR16 *GetMemoryTypeUnicode(EFI_MEMORY_TYPE type)
+{
+    switch (type)
+    {
+    case EfiReservedMemoryType:
+        return L"EfiReservedMemoryType";
+    case EfiLoaderCode:
+        return L"EfiLoaderCode";
+    case EfiLoaderData:
+        return L"EfiLoaderData";
+    case EfiBootServicesCode:
+        return L"EfiBootServicesCode";
+    case EfiBootServicesData:
+        return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode:
+        return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData:
+        return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory:
+        return L"EfiConverntionalMemory";
+    case EfiUnusableMemory:
+        return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory:
+        return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS:
+        return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO:
+        return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace:
+        return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode:
+        return L"EfiPalCode";
+    case EfiPersistentMemory:
+        return L"EfiPersistentMemory";
+    case EfiMaxMemoryType:
+        return L"EfiMaxMemoryType";
+    default:
+        return L"InvalidMemoryType";
+    }
+}
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table)
 {
     Print(L"Hello, Mikan World!\n");
+
+    CHAR8 memmap_buf[4096 * 4];
+    struct MemoryMap memmap = {
+        sizeof(memmap_buf),
+        memmap_buf,
+        0,
+        0,
+        0,
+        0,
+    };
+    GetMemoryMap(&memmap);
+
+    EFI_FILE_PROTOCOL *root_dir;
+
     while (1)
         ;
     return EFI_SUCCESS;
